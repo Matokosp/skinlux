48b840d335fb427c5c3d5756e78c398bddeccd5e
Matokosp <jmsalinas@uc.cl>
2019-06-19 22:52:19 -0400

    update

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,14 +1,51 @@
 // Sticky
+//
+// window.onscroll = function() {myFunction()};
+//
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+//
+// function myFunction() {
+//   if (document.getElementById("contentDos").offsetTop >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
-window.onscroll = function() {myFunction()};
+navbar = $('#navbar');
+section = $('#hola').offset();
+sticky = navbar.offset();
+var $indow = $(window);
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// $indow.scroll(function () {
+//   if ($indow.scrollTop() >= section.top ) {
+//     navbar.addClass('sticky');
+//   } else {
+//     navbar.removeClass('sticky');
+//   }
+// })
+//
+// $indow.scroll(function () {
+//   console.log('dah');
+// })
+
+$(window).scroll(function() {
+    if (section.top >= 100) {
+        //custom code
+        navbar.addClass('sticky');
+    }
+});
+
+
+// $(window).on('scroll', function() {
+//     scrollPosition = $(this).scrollTop();
+//     if (scrollPosition >= 500) {
+//         // If the function is only supposed to fire once
+//         $(this).off('scroll');
+//          navbar.addClass('sticky');
+//
+//         // Other function stuff here...
+//     }
+// });
